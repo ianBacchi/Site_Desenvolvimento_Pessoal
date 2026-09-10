8e50ad86c1d87964214dc73649bb65ddb4a106ff
Ian Bacchi Nascimento <120351723+ianBacchi@users.noreply.github.com>
2026-09-10 17:36:24 -0300

    fix: reorder LatestPosts component position in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,9 +17,9 @@ export default function HomePage() {
         <About /> {/* Quote section */}
         <Stats />
         <Trajetoria />
-        <LatestPosts />
         <Services />
         <CTA />
+        <LatestPosts />
         <Testimonials />
       </main>
       <Footer />
